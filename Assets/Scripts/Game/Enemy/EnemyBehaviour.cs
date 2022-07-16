56b39a06d480ce71d04e0625835a8983237f0a55
ENRIKE <erikghast@gmail.com>
2022-07-17 00:34:41 +0200

    Back to A*
    After a research I decided to stick with the A* Pathfinding.. Hope it works.

--- Assets/Scripts/Game/Enemy/EnemyBehaviour.cs
+++ Assets/Scripts/Game/Enemy/EnemyBehaviour.cs
@@ -4,80 +4,13 @@ using UnityEngine;
 
 public class EnemyBehaviour : MonoBehaviour
 {
-    private Rigidbody2D rb;
-    public Transform target;
-
-    public GameObject tempTarget;
-    public Structure structureScript;
-
-    private int damage = 5;
-
-    private float damageCooldown = 2f;
-    private float damageTime;
-    private void Awake()
-    {
-
-        if (target == null)
-        {
-            
-        }
-    }
+    
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        damageTime = Time.time + damageCooldown;
-
-        try
-        {
-            target = FindObjectOfType<TownHallStructure>().transform;
-            if (target == null)
-            {
-                target = GameObject.FindWithTag("Player").transform;
-            }
-        }
-        catch
-        {
-            Debug.Log("Town hall or Player not found");
-        }
+        
     }
     void Update()
     {
-        if (tempTarget == null)
-        {
-            transform.LookAt(target.position);
-            rb.MovePosition(target.position);
-            
-        }
-        else
-        {
-            structureScript = tempTarget.GetComponent<Structure>();
-            transform.LookAt(tempTarget.transform.position);
-            if (Time.time > damageTime)
-            {
-                Damage();
-            }
-        }
-        
-    }
-    
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.collider.GetComponent<IDamageToFriendly>() != null)
-        {
-            tempTarget = collision.collider.gameObject;
-        }
-    }
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.collider.GetComponent<IDamageToFriendly>() != null)
-        {
-            tempTarget = null;
-            structureScript = null;
-        }
-    }
-    private void Damage()
-    {
-        damageTime = Time.time + damageCooldown;
-        structureScript.StructureHealth -= damage;
+
     }
 }
