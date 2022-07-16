dec6c94b5eaa1a77d19c34f72b178b0bf8a7b361
ENRIKE <erikghast@gmail.com>
2022-07-16 22:14:37 +0200

    Enemy
    No A* Pathfinding
    Enemy Movement not working yet

--- Assets/Scripts/Game/Enemy/EnemyBehaviour.cs
+++ Assets/Scripts/Game/Enemy/EnemyBehaviour.cs
@@ -4,15 +4,80 @@ using UnityEngine;
 
 public class EnemyBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody2D rb;
+    public Transform target;
+
+    public GameObject tempTarget;
+    public Structure structureScript;
+
+    private int damage = 5;
+
+    private float damageCooldown = 2f;
+    private float damageTime;
+    private void Awake()
+    {
+
+        if (target == null)
+        {
+            
+        }
+    }
     void Start()
     {
-        
-    }
+        rb = GetComponent<Rigidbody2D>();
+        damageTime = Time.time + damageCooldown;
 
-    // Update is called once per frame
+        try
+        {
+            target = FindObjectOfType<TownHallStructure>().transform;
+            if (target == null)
+            {
+                target = GameObject.FindWithTag("Player").transform;
+            }
+        }
+        catch
+        {
+            Debug.Log("Town hall or Player not found");
+        }
+    }
     void Update()
     {
+        if (tempTarget == null)
+        {
+            transform.LookAt(target.position);
+            rb.MovePosition(target.position);
+            
+        }
+        else
+        {
+            structureScript = tempTarget.GetComponent<Structure>();
+            transform.LookAt(tempTarget.transform.position);
+            if (Time.time > damageTime)
+            {
+                Damage();
+            }
+        }
         
+    }
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.GetComponent<IDamageToFriendly>() != null)
+        {
+            tempTarget = collision.collider.gameObject;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.GetComponent<IDamageToFriendly>() != null)
+        {
+            tempTarget = null;
+            structureScript = null;
+        }
+    }
+    private void Damage()
+    {
+        damageTime = Time.time + damageCooldown;
+        structureScript.StructureHealth -= damage;
     }
 }
